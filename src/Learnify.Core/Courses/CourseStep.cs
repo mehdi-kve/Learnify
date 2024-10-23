349479fa5912f78c2a2677b3beee8584875bef7a
Admin <admin@gmail.com>
2024-10-23 09:18:16 +0330

    Add Entity

---
+++ src/Learnify.Core/Courses/CourseStep.cs
@@ -0,0 +1,31 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using Abp.Timing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learnify.Courses
+{
+    public class CourseStep : Entity, IHasCreationTime
+    {
+        public int Progress { get; set; }
+        public DateTime CreationTime { get; set; }
+
+        /*[ForeignKey(nameof(AssignedPersonId))]
+        public Person AssignedPerson { get; set; }
+        public Guid? AssignedPersonId { get; set; }
+
+        [ForeignKey(nameof(AssignedPersonId))]
+        public Person AssignedPerson { get; set; }
+        public Guid? AssignedPersonId { get; set; }*/
+
+        public CourseStep()
+        {
+            CreationTime = Clock.Now;
+        }
+    }
+}
---
+++ src/Learnify.Core/Students/Student.cs
@@ -0,0 +1,29 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using Abp.Timing;
+using Learnify.Courses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learnify.Students
+{
+    [Table("Students")]
+    public class Student : Entity, IHasCreationTime
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        [Column("RegistrationDate")]
+        public DateTime CreationTime { get; set; }
+
+        public IList<CourseStep> CourseSteps { get; set;}
+
+        public Student()
+        {
+            CreationTime = Clock.Now;
+        }
+    }
+}

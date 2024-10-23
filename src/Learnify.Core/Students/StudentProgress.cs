664d64676d8ab27578d361e0f15e36ba3488d64e
Admin <admin@gmail.com>
2024-10-23 11:42:28 +0330

    Add DbSets

--- src/Learnify.Core/Students/StudentProgress.cs
+++ src/Learnify.Core/Students/StudentProgress.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Learnify.Students
 {
-    [Table("StudentsProgress")]
+    [Table("StudentProgresses")]
     public class StudentProgress: Entity
     {
         public ProgressState State { get; set; }
--- src/Learnify.EntityFrameworkCore/EntityFrameworkCore/LearnifyDbContext.cs
+++ src/Learnify.EntityFrameworkCore/EntityFrameworkCore/LearnifyDbContext.cs
@@ -3,6 +3,9 @@ using Abp.Zero.EntityFrameworkCore;
 using Learnify.Authorization.Roles;
 using Learnify.Authorization.Users;
 using Learnify.MultiTenancy;
+using Learnify.Students;
+using Learnify.Courses;
+using Learnify.Enrollments;
 
 namespace Learnify.EntityFrameworkCore
 {
@@ -10,6 +13,12 @@ namespace Learnify.EntityFrameworkCore
     {
         /* Define a DbSet for each entity of the application */
         
+        public DbSet<Student> students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<CourseStep> CourseSteps { get; set; }
+        public DbSet<StudentProgress> studentProgresses { get; set; }
+
         public LearnifyDbContext(DbContextOptions<LearnifyDbContext> options)
             : base(options)
         {

9da54e08b193f6378907b9fdfbec12e6818a645e
Admin <admin@gmail.com>
2024-11-06 08:57:53 +0330

    Update Permission Names

--- src/Learnify.Core/Authorization/PermissionNames.cs
+++ src/Learnify.Core/Authorization/PermissionNames.cs
@@ -8,6 +8,9 @@
         public const string Pages_Users_Activation = "Pages.Users.Activation";
 
         public const string Pages_Roles = "Pages.Roles";
+
+        public const string Pages_Student = "Pages.Student";
+        public const string Pages_Student_Access = "Pages.Student.Access";
     }
 }
     
